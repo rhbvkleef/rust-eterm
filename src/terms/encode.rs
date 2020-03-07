76db321e12709f604ebbde48bd240bbd3f0a818f
rhbvkleef <rolf@vankleef.me>
2020-03-07 13:30:18 +0100

    Refactor tests for integer primitive encoding

--- src/terms/encode.rs
+++ src/terms/encode.rs
@@ -127,11 +127,7 @@ impl ToExternalBinary for i128 {
         if *self <= i32::max_value().into() && *self >= i32::min_value().into() {
             (*self as i32).to_writer(writer)
         } else {
-            let mut abs = if *self == i128::min_value() {
-                0x80_00_00_00_00_00_00_00_00_00_00_00_00_00_00_00u128
-            } else {
-                self.abs() as u128
-            };
+            let mut abs = lossless_abs(*self);
 
             let bytes: u8 = (16 - (abs.leading_zeros() >> 3)) as u8;
             let sign: u8 = if *self >= 0 { 0 } else { 1 };
@@ -433,72 +429,142 @@ fn write(writer: &mut dyn Write, bytes: &[u8]) -> Result<usize, Error> {
     }
 }
 
+fn lossless_abs(num: i128) -> u128 {
+    if num >= 0 {
+        num as u128
+    } else if num == i128::min_value() {
+        0x80_00_00_00_00_00_00_00_00_00_00_00_00_00_00_00u128
+    } else {
+        -num as u128
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::ETerm;
+    use super::lossless_abs;
 
-    #[test]
-    fn u8_to_binary() {
-        assert_eq!(vec![97, 0], u8::min_value().try_to_external_binary().unwrap());
-        assert_eq!(vec![97, 255], u8::max_value().try_to_external_binary().unwrap());
+    #[derive(Debug)]
+    struct Test {
+        binary: Vec<u8>,
+        is_negative: bool,
+        number: u128,
     }
 
-    #[test]
-    fn i8_to_binary() {
-        assert_eq!(vec![98, 255, 255, 255, 128], i8::min_value().try_to_external_binary().unwrap());
-        assert_eq!(vec![97, 127], i8::max_value().try_to_external_binary().unwrap());
+    macro_rules! test {
+        ($bin:tt, false, $val:expr) => {
+            &Test::new(vec!$bin, false, $val as u128)
+        };
+        ($bin:tt, true, $val:expr) => {
+            &Test::new(vec!$bin, true, lossless_abs($val as i128))
+        };
     }
 
-    #[test]
-    fn u16_to_binary() {
-        assert_eq!(vec![97, 0], u16::min_value().try_to_external_binary().unwrap());
-        assert_eq!(vec![98, 0, 0, 255, 255], u16::max_value().try_to_external_binary().unwrap());
+    impl Test {
+        fn new(binary: Vec<u8>, is_negative: bool, number: u128) -> Test {
+            Test {
+                binary,
+                is_negative,
+                number,
+            }
+        }
+
+        fn run_all(signed: bool, bits: u8) {
+            for test in &[
+                test!([97, 0], false, 0),
+
+                // i8 and u8 type boundaries
+                test!([98, 255, 255, 255, 128], true, i8::min_value()),
+                test!([97, 127], false, i8::max_value()),
+                test!([97, 255], false, u8::max_value()),
+
+                // i16 and u16 type boundaries
+                test!([98, 255,255, 128, 0], true, i16::min_value()),
+                test!([98, 0, 0, 127, 255], false, i16::max_value()),
+                test!([98, 0, 0, 255, 255], false, u16::max_value()),
+
+                // i32 and u32 type boundaries
+                test!([98, 128, 0, 0, 0], true, i32::min_value()),
+                test!([98, 127, 255, 255, 255], false, i32::max_value()),
+                test!([110, 4, 0, 255, 255, 255, 255], false, u32::max_value()),
+
+                // i64 and u64 type boundaries
+                test!([110, 8, 1, 0, 0, 0, 0, 0, 0, 0, 128], true, i64::min_value()),
+                test!([110, 8, 0, 255, 255, 255, 255, 255, 255, 255, 127], false, i64::max_value()),
+                test!([110, 8, 0, 255, 255, 255, 255, 255, 255, 255, 255], false, u64::max_value()),
+
+                // i128 and u128 type boundaries
+                test!([110, 16, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 128], true, i128::min_value()),
+                test!([110, 16, 0, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 127], false, i128::max_value()),
+                test!([110, 16, 0, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255], false ,u128::max_value()),
+            ] {
+
+                test.run(signed, bits);
+            }
+        }
+
+        fn run(&self, signed: bool, bits: u8) {
+            let bin = match (signed, bits, self.is_negative, self.number) {
+                (true, 8, s, n) if n <= (i8::max_value() as u128) || (s && n <= lossless_abs(i8::min_value() as i128)) =>
+                    (self.with_sign() as i8).try_to_external_binary(),
+                (false, 8, false, n) if n <= (u8::max_value() as u128) =>
+                    (self.number as u8).try_to_external_binary(),
+                (true, 16, s, n) if n <= (i16::max_value() as u128) || (s && n <= lossless_abs(i16::min_value() as i128)) =>
+                    (self.with_sign() as i16).try_to_external_binary(),
+                (false, 16, false, n) if n <= (u16::max_value() as u128) =>
+                    (self.number as u16).try_to_external_binary(),
+                (true, 32, s, n) if n <= (i32::max_value() as u128) || (s && n <= lossless_abs(i32::min_value() as i128)) =>
+                    (self.with_sign() as i32).try_to_external_binary(),
+                (false, 32, false, n) if n <= (u32::max_value() as u128) =>
+                    (self.number as u32).try_to_external_binary(),
+                (true, 64, s, n) if n <= (i64::max_value() as u128) || (s && n <= lossless_abs(i64::min_value() as i128)) =>
+                    (self.with_sign() as i64).try_to_external_binary(),
+                (false, 64, false, n) if n <= (u64::max_value() as u128) =>
+                    (self.number as u64).try_to_external_binary(),
+                (true, 128, s, n) if n <= (i128::max_value() as u128) || (s && n <= lossless_abs(i128::min_value() as i128)) =>
+                    (self.with_sign() as i128).try_to_external_binary(),
+                (false, 128, false, n) if n <= u128::max_value() =>
+                    self.number.try_to_external_binary(),
+                _ => return,
+            }.unwrap();
+
+            assert_eq!(self.binary, bin, "Call mode: {{signed: {}, bit_width: {}}}, Test mode: {{negative: {}, number: {}}}", signed, bits, self.is_negative, self.number);
+        }
+
+        fn with_sign(&self) -> i128 {
+            if self.is_negative && self.number == lossless_abs(i128::min_value()) {
+                i128::min_value()
+            } else if self.is_negative {
+                -(self.number as i128)
+            } else {
+                self.number as i128
+            }
+        }
     }
 
-    #[test]
-    fn i16_to_binary() {
-        assert_eq!(vec![98, 255,255, 128, 0], i16::min_value().try_to_external_binary().unwrap());
-        assert_eq!(vec![98, 0, 0, 127, 255], i16::max_value().try_to_external_binary().unwrap());
+    macro_rules! maketest {
+        ($name:ident, unsigned, $bits:expr) => (
+            #[test]
+            fn $name() {
+                Test::run_all(false, $bits);
+            }
+        );
+        ($name:ident, signed, $bits:expr) => (
+            #[test]
+            fn $name() {
+                Test::run_all(true, $bits);
+            }
+        );
     }
 
-    #[test]
-    fn u32_to_binary() {
-        assert_eq!(vec![97, 0], u32::min_value().try_to_external_binary().unwrap());
-        assert_eq!(vec![110, 4, 0, 255, 255, 255, 255], u32::max_value().try_to_external_binary().unwrap());
-    }
-
-    #[test]
-    fn i32_to_binary() {
-        assert_eq!(vec![98, 128, 0, 0, 0], i32::min_value().try_to_external_binary().unwrap());
-        assert_eq!(vec![98, 127, 255, 255, 255], i32::max_value().try_to_external_binary().unwrap());
-    }
-
-    #[test]
-    fn u64_to_binary() {
-        assert_eq!(vec![97, 0], u64::min_value().try_to_external_binary().unwrap());
-        assert_eq!(vec![110, 8, 0, 255, 255, 255, 255, 255, 255, 255, 255], u64::max_value().try_to_external_binary().unwrap());
-    }
-
-    #[test]
-    fn i64_to_binary() {
-        assert_eq!(vec![110, 8, 1, 0, 0, 0, 0, 0, 0, 0, 128], i64::min_value().try_to_external_binary().unwrap());
-        assert_eq!(vec![110, 8, 0, 255, 255, 255, 255, 255, 255, 255, 127], i64::max_value().try_to_external_binary().unwrap());
-    }
-
-    #[test]
-    fn u128_to_binary() {
-        assert_eq!(vec![97, 0], u128::min_value().try_to_external_binary().unwrap());
-        assert_eq!(
-            vec![110, 16, 0, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255],
-            u128::max_value().try_to_external_binary().unwrap());
-    }
-
-    #[test]
-    fn i128_to_binary() {
-        assert_eq!(vec![110, 16, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 128],
-            i128::min_value().try_to_external_binary().unwrap());
-        assert_eq!(
-            vec![110, 16, 0, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 127],
-            i128::max_value().try_to_external_binary().unwrap());
-    }
+    maketest!(u8, unsigned, 8);
+    maketest!(i8, signed, 8);
+    maketest!(u16, unsigned, 16);
+    maketest!(i16, signed, 16);
+    maketest!(u32, unsigned, 32);
+    maketest!(i32, signed, 32);
+    maketest!(u64, unsigned, 64);
+    maketest!(i64, signed, 64);
+    maketest!(u128, unsigned, 128);
+    maketest!(i128, signed, 128);
 }
